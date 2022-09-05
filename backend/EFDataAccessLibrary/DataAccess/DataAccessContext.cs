a2f8885d77b460a9aea7f521d35f753c35ede60f
tranhai3112 <tranvanhai31122000@gmail.com>
2022-09-05 14:11:50 +0700

    add DataAccessContext

---
+++ backend/EFDataAccessLibrary/DataAccess/DataAccessContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public class DataAccessContext : DbContext
+    {
+        public DataAccessContext(DbContextOptions<DataAccessContext> options) : base(options) { }
+
+
+    }
+}
